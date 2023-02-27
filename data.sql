5638e211ebaf9c7b730e30049e7402db6bf15e88
EddyShimwa <shedpray16@gmail.com>
2023-02-27 17:20:41 +0200

    added the data in the tables

--- data.sql
+++ data.sql
@@ -80,6 +80,9 @@ INSERT INTO visits (animal_id, vet_id, visited_date)
   (10, 1, '2021-01-11');
 
 
+INSERT INTO visits (animal_id, vet_id, visited_date) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
 
 
 
--- schema.sql
+++ schema.sql
@@ -48,3 +48,4 @@ CREATE TABLE visits (
 );
 
 
+
